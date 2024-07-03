2ba293e8e1d85da75439d2d51d90556a023f4625
Nicholas Roma <nikoandpiko@gmail.com>
2024-07-03 22:25:48 +0900

    Updates to styling

--- src/components/DescendantList.svelte
+++ src/components/DescendantList.svelte
@@ -11,22 +11,25 @@
 <div class="metadata-section" id="descendant-data">
   <h2>Descendants</h2>
   {#if data.length > 0}
-    <ul>
+    <ul class="descendant-list">
       {#each data as descendant}
         <li>
           <strong>{descendant.descendant_name}</strong><br>
           <img class="descendant-image" src={descendant.descendant_image_url} alt={descendant.descendant_name} loading="lazy" />
-          <div>
-            <h3>Skills</h3>
-            <ul>
-              {#each descendant.descendant_skill as skill}
-                <li>
-                  <strong>{skill.skill_name}</strong><br>
-                  Type: {skill.skill_type}<br>
-                  Element: {skill.element_type}<br>
-                  Archetype: {skill.arche_type}<br>
-                  Description: {skill.skill_description}<br>
-                  <img class="skill-image" src={skill.skill_image_url} alt={skill.skill_name} loading="lazy" />
+          <div class="skills">
+            <ul class="skill-list">
+              {#each descendant.descendant_skill as skill, index}
+                <li class="skill-item">
+                  <span class="skill-tooltip">
+                    <img class="skill-image" src={skill.skill_image_url} alt={skill.skill_name} loading="lazy" />
+                    <strong>{skill.skill_name}</strong>
+                    <span class="tooltip-text">
+                      Type: {skill.skill_type}<br>
+                      Element: {skill.element_type}<br>
+                      Archetype: {skill.arche_type}<br>
+                      Description: {skill.skill_description}
+                    </span>
+                  </span>
                 </li>
               {/each}
             </ul>
@@ -35,15 +38,19 @@
       {/each}
     </ul>
   {:else}
-    <div>Loading descendant data...</div>
+    <div>Loading Descendant data...</div>
   {/if}
 </div>
 
 <style>
-  ul {
+  ul.descendant-list {
     list-style-type: none;
     padding: 0;
+    display: grid;
+    gap: 15px;
+    grid-template-columns: repeat(1, 1fr);
   }
+
   li {
     margin-bottom: 15px;
     background: #1a1a1a;
@@ -56,14 +63,75 @@
     align-items: center;
     text-align: center;
   }
+
   .descendant-image {
     margin: 10px 0;
     width: 256px;
     height: 256px;
+    border-radius: 50%;
   }
+
+  .skills {
+    width: 100%;
+  }
+
+  .skill-list {
+    list-style-type: none;
+    padding: 0;
+    display: flex;
+    flex-wrap: wrap;
+    gap: 10px;
+    justify-content: center;
+  }
+
+  .skill-item {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    text-align: center;
+    width: 30%;
+  }
+
+  .skill-tooltip {
+    position: relative;
+    padding: 0.5em;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    cursor: pointer;
+  }
+
   .skill-image {
-    margin: 10px 0;
-    width: 128px;
-    height: 128px;
+    width: 96px;
+    height: 96px;
+    border-radius: 50%;
+  }
+
+  .tooltip-text {
+    padding: 0.5em;
+    visibility: hidden;
+    width: 220px;
+    background-color: #555;
+    color: #fff;
+    text-align: center;
+    border-radius: 6px;
+    position: absolute;
+    z-index: 1;
+    bottom: 125%;
+    left: 50%;
+    margin-left: -110px;
+    opacity: 0;
+    transition: opacity 0.3s;
+  }
+
+  .skill-tooltip:hover .tooltip-text {
+    visibility: visible;
+    opacity: 1;
+  }
+
+  @media (min-width: 768px) {
+    ul.descendant-list {
+      grid-template-columns: repeat(2, 1fr);
+    }
   }
 </style>
--- src/components/WeaponList.svelte
+++ src/components/WeaponList.svelte
@@ -14,12 +14,24 @@
     <ul>
       {#each data as weapon}
         <li>
-          <strong>{weapon.weapon_name}</strong><br>
-          Type: {weapon.weapon_type}<br>
-          Rarity: {weapon.weapon_tier}<br>
-          Rounds Type: {weapon.weapon_rounds_type}<br>
-          Perk Ability: {weapon.weapon_perk_ability_name ? `${weapon.weapon_perk_ability_name} - ${weapon.weapon_perk_ability_description}` : 'None'}<br>
-          <img src={weapon.image_url} alt={weapon.weapon_name} loading="lazy" />
+          <div class="weapon-details">
+            <strong>{weapon.weapon_name}</strong>
+            <span>Type: {weapon.weapon_type}</span>
+            <span>Rarity: {weapon.weapon_tier}</span>
+            <span>Rounds Type: {weapon.weapon_rounds_type}</span>
+            <span class="perk">
+              Perk Ability: {weapon.weapon_perk_ability_name ? weapon.weapon_perk_ability_name : 'None'}
+              {#if weapon.weapon_perk_ability_name}
+                <span class="perk-tooltip">
+                  <img src="/images/question-mark.svg" alt="?" class="icon">
+                  <span class="tooltip-text">{weapon.weapon_perk_ability_description}</span>
+                </span>
+              {/if}
+            </span>
+          </div>
+          <div class="weapon-image">
+            <img src={weapon.image_url} alt={weapon.weapon_name} loading="lazy" />
+          </div>
         </li>
       {/each}
     </ul>
@@ -32,9 +44,11 @@
   ul {
     list-style-type: none;
     padding: 0;
+    display: grid;
+    gap: 15px;
+    grid-template-columns: repeat(1, 1fr);
   }
   li {
-    margin-bottom: 15px;
     background: #1a1a1a;
     color: #fff;
     padding: 15px;
@@ -42,13 +56,86 @@
     box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
     display: flex;
     flex-direction: column;
+    justify-content: space-between;
     align-items: center;
     text-align: center;
+    position: relative;
   }
-  li > img {
-    margin: 10px 0;
-    width: 1024px;
-    height: 304px;
+  .weapon-details {
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    margin-bottom: 10px;
+    flex-grow: 1;
+  }
+  .weapon-details span {
+    margin: 5px 0;
+  }
+  .perk {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+  .perk-tooltip {
+    position: relative;
+    display: inline-flex;
+    align-items: center;
+    cursor: pointer;
+    margin-left: 5px;
+    height: 16px;
+    width: 16px;
+  }
+  .perk-tooltip .icon {
+    width: 16px;
+    height: 16px;
+    margin-left: 5px;
+    background-color: #fff;
+    border-radius: 50%;
+    padding: 2px;
+  }
+  .perk-tooltip .tooltip-text {
+    padding: 0.5em;
+    visibility: hidden;
+    width: 220px;
+    background-color: #555;
+    color: #fff;
+    text-align: center;
+    border-radius: 6px;
+    position: absolute;
+    z-index: 1;
+    bottom: 125%;
+    left: 50%;
+    margin-left: -110px;
+    opacity: 0;
+    transition: opacity 0.3s;
+  }
+  .perk-tooltip:hover .tooltip-text {
+    visibility: visible;
+    opacity: 1;
+  }
+  .weapon-image {
+    width: 100%;
+    display: flex;
+    justify-content: center;
+  }
+  .weapon-image img {
+    width: 100%;
     height: auto;
+  }
+
+  @media (min-width: 480px) {
+    ul {
+      grid-template-columns: repeat(2, 1fr);
+    }
+  }
+  @media (min-width: 768px) {
+    ul {
+      grid-template-columns: repeat(3, 1fr);
+    }
+  }
+  @media (min-width: 1024px) {
+    ul {
+      grid-template-columns: repeat(4, 1fr);
+    }
   }
 </style>
